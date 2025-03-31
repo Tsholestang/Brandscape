f18e116e80ef1af388a5851805bb4c4f9fd5cebf
Tsholo <70982100+Tsholestang@users.noreply.github.com>
2025-03-31 16:11:44 +0200

    Add files via upload

---
+++ bdslll.js
@@ -0,0 +1,41 @@
+
+// Page navigation
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+
+/* slideshow quotes section */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+	
+slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}	
+
